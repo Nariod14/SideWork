b494df3cfe53963db44991b0f29b403e17b48a28
Usmi Mukherjee <us964214@dal.ca>
2023-10-18 19:06:29 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.1.3" apply false
+    id("com.android.application") version "8.1.1" apply false
     id ("com.google.gms.google-services") version "4.4.0" apply false
 }
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.1.3")
 
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
