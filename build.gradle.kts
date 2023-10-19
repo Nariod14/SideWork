b075d4fb37496e60f1a904516ac2dce810153097
Usmi Mukherjee <us964214@dal.ca>
2023-10-19 13:06:28 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20-RC")
-        classpath("com.android.tools.build:gradle:7.0.0")
+        classpath("com.android.tools.build:gradle:7.1.2")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.1" apply false
+    id("com.android.application") version "7.1.2" apply false
+    id("com.android.library") version "7.1.2" apply false
     id ("com.google.gms.google-services") version "4.4.0" apply false
 }
