7aba6c303102b8977a72a1188238f56d992adf3b
Nari <Nariod14@gmail.com>
2023-11-27 08:28:55 -0400

    Edited profiles to no longer show the rating when viewing self. Fixed several bugs.

--- app/src/main/java/com/example/quickcashcsci3130g_11/EmployerProfileActivity.java
+++ app/src/main/java/com/example/quickcashcsci3130g_11/EmployerProfileActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -12,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,12 +28,15 @@ public class EmployerProfileActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private String employerId;
     private TextView mDisplayNameTextView;
+    private ImageButton backButton;
     private RatingBar mRatingBar;
     private EditText mCommentsEditText;
     private Button mSubmitRatingButton;
 
     private RecyclerView mRatingsRecyclerView;
     private RatingsAdapter mRatingsAdapter;
+
+    private String currentUserID;
 
 
     @Override
@@ -39,12 +45,15 @@ public class EmployerProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_employer_profile);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        employerId = "employer123"; // Replace with the actual employer's ID
+        employerId = getIntent().getStringExtra("inputUID");
+        currentUserID = getIntent().getStringExtra("currentUserID");
+
 
         mDisplayNameTextView = findViewById(R.id.displayNameTextView);
         mRatingBar = findViewById(R.id.ratingBar);
         mCommentsEditText = findViewById(R.id.commentsEditText);
         mSubmitRatingButton = findViewById(R.id.submitRatingButton);
+        backButton = findViewById(R.id.backButtonEmployerProfile);
 
         mRatingsRecyclerView = findViewById(R.id.ratingsRecyclerView);
         mRatingsAdapter = new RatingsAdapter();
@@ -57,12 +66,30 @@ public class EmployerProfileActivity extends AppCompatActivity {
         // Retrieve and display the employer's ratings
         retrieveEmployerRatings(employerId);
 
+
+
+
+        if (currentUserID.equals(employerId)) {
+            // Hide the RatingBar, EditText, and Button
+            findViewById(R.id.ratingBar).setVisibility(View.GONE);
+            findViewById(R.id.commentsEditText).setVisibility(View.GONE);
+            findViewById(R.id.submitRatingButton).setVisibility(View.GONE);
+        }
+
         mSubmitRatingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 float ratingValue = mRatingBar.getRating();
                 String comments = mCommentsEditText.getText().toString();
                 submitRating(employerId, ratingValue, comments);
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
             }
         });
     }
@@ -107,18 +134,33 @@ public class EmployerProfileActivity extends AppCompatActivity {
     }
 
     private void updateUIWithEmployerInformation(User employer) {
-        // Assuming you have a TextView in your layout with the ID displayNameTextView
-        TextView displayNameTextView = findViewById(R.id.displayNameTextView);
-        displayNameTextView.setText(employer.getDisplayName());
-        // You can similarly update other UI elements with additional employer information
-        // For example: findViewById(R.id.emailTextView).setText(employer.getEmail());
+        mDisplayNameTextView.setText(employer.getDisplayName());
     }
 
     private void submitRating(String employerId, float ratingValue, String comments) {
         // Create a new Rating object
+        String currentUserID = FirebaseAuth.getInstance().getCurrentUser().getUid();
         Rating rating = new Rating(employerId, ratingValue, comments);
 
-        // Store the new rating in the Firebase database
-        mDatabase.child("employerRatings").child(employerId).push().setValue(rating);
+        mDatabase.child("employeeRatings").child(currentUserID).child(employerId)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        User employer = dataSnapshot.getValue(User.class);
+                        if (employer != null) {
+                            Snackbar.make(findViewById(R.id.backButtonEmployerProfile), "You've already submitted a review!", Snackbar.LENGTH_SHORT).show();
+                        } else
+                            // Store the new rating in the Firebase database
+                            mDatabase.child("employerRatings").child(employerId).push().setValue(rating);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        // Handle the error
+                    }
+                });
+
+
+
     }
 }
