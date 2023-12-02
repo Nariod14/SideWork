b7de96014222fdb4ac5db0e174515b5f40ab813a
Rujul Patel <rujulpatel1@gmail.com>
2023-12-02 04:33:23 -0400

    Add Job to Favourite Done.

---
+++ app/src/main/java/com/example/quickcashcsci3130g_11/FavouriteJobActivity.java
@@ -0,0 +1,69 @@
+package com.example.quickcashcsci3130g_11;
+
+import static android.content.ContentValues.TAG;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageButton;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FavouriteJobActivity extends AppCompatActivity {
+    private JobListAdapter mAdapter;
+    private List<Job> mJobList;
+    private DatabaseReference mDatabase;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        RecyclerView mRecyclerView;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourite_jobs);
+
+        initializeBackButton();
+
+        // Initialize the RecyclerView and its adapter
+        mRecyclerView = findViewById(R.id.favouriteJobRecyclerView);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mJobList = new ArrayList<>();
+        mAdapter = new JobListAdapter(mJobList);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Initialize the Firebase database reference
+        mDatabase = FirebaseDatabase.getInstance().getReference("Favourite");
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot jobSnapshot : dataSnapshot.getChildren()) {
+                    Job job = jobSnapshot.getValue(Job.class);
+                    mJobList.add(job);
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e(TAG, "onCancelled", databaseError.toException());
+            }
+        });
+    }
+
+    private void initializeBackButton() {
+        ImageButton backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(FavouriteJobActivity.this, EmployeeActivity.class);
+            startActivity(intent);
+        });
+    }
+}
