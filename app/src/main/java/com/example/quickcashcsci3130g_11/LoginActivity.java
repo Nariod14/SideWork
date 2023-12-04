d5d2b57e0882c46318afe4f928a2c6aa20e2a171
betsymccord <bt816091@dal.ca>
2023-12-04 15:08:37 -0400

    Added toolbar and menu for Reputation. Login password hidden.

--- app/src/main/java/com/example/quickcashcsci3130g_11/LoginActivity.java
+++ app/src/main/java/com/example/quickcashcsci3130g_11/LoginActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -80,6 +81,7 @@ public class LoginActivity extends AppCompatActivity {
     private void loginUser() {
         String email = emailEditText.getText().toString();
         String password = passwordEditText.getText().toString();
+        passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
 
         if (email.equals("") || password.equals("")) {
             Toast.makeText(LoginActivity.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
